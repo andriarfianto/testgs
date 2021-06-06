6c7c4f01764606f037b0c8b577c915785a74e097
Andri Arfianto <aarfianto11@gmail.com>
2021-06-06 17:21:25 +0700

    Update my.js

--- my.js
+++ my.js
@@ -1,7 +1,7 @@
 function testGS() {
-    
+
     var url = "https://script.google.com/macros/s/AKfycbwsKKB6PeqqeeMlBEnyuiEozeOdtJkzHgc8SjJ2V4XPfNo7fJwbuu4eWXOYdL7VPYrCOQ/exec";
-    
+
     fetch(url)
         .then(d => d.json())
         .then(d => {
@@ -9,4 +9,22 @@ function testGS() {
         });
 }
 
+function addGS() {
+
+    var url = "https://script.google.com/macros/s/AKfycbxdZZ5STIKmNzhpwRAPHvQU3JVEkDSDMI_OoS1EsYayPCYAmXNzBMTVXoqcAyvFIR4/exec";
+
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name: "Doe"}) // body data type must match "Content-Type" header
+    })
+}
+
+document.getElementById("btn2").addEventListener('click', addGS());
 document.getElementById("btn").addEventListener('click', testGS());
